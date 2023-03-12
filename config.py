79aaa551182eb8ddef9a79876a2aa84c61a46ab2
qadirnesirov02 <123129882+Qadirnesirov@users.noreply.github.com>
2023-03-12 21:37:17 +0400

    update config.py

--- config.py
+++ config.py
@@ -14,7 +14,7 @@ client = TelegramClient('client', api_id, api_hash).start(bot_token=bot_token)
 USERNAME = "Rahid_Tag_Bot"
 log_qrup = -1001864613336
 startmesaj = "\n\nMən qrupda bütün istifadəçiləri çağırmaq üçün tağ botuyam 🥰\n\nMəni qrupa əlavə edin sonra admin edərək tağ prosesi başlada bilərsən 👀"
-komutlar = "» /utag > 5 nəfərlik tağ edərəm 🙊\n\n» /tag > Təkli tağ edərəm 🦦\n\n» /atag > Qrup adminlərini tağ edərəm 😁\n\n» /etag > Emojilər ilə tağ edərəm 🥰\n\n» /stag > Maraqlı sözlərlə tağ edərəm 😍\n\n» /cancel > Tağ prosesi dayandıraram 👀"
+komutlar = "» /tag > 5 nəfərlik tağ edərəm 🙊\n\n» /tektag > Təkli tağ edərəm 🦦\n\n» /atag > Qrup adminlərini tağ edərəm 😁\n\n» /etag > Emojilər ilə tağ edərəm 🥰\n\n» /stag > Maraqlı sözlərlə tağ edərəm 😍\n\n» /cancel > Tağ prosesi dayandıraram 👀"
 qrupstart = "Mən işləyirəm 🥰\n\nBot əmrləri öyrənmək üçün şəxsimdə mənə yazın 👀"
 support = "qruzdaa"
 sahib = "Rahid_7"
