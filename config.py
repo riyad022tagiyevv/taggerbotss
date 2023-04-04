eba1a0527486b363465b306cd40212aa6c95f752
qadirnesirov02 <123129882+Qadirnesirov@users.noreply.github.com>
2023-04-05 03:54:49 +0400

    update config.py

--- config.py
+++ config.py
@@ -6,18 +6,18 @@ from telethon import TelegramClient, events, Button
 # 
 api_id = 19485442
 api_hash = "a03fcb372b3ec4e406b5d52f84b02e53"
-bot_token = "5394785524:AAEozSRqSyD2vVtvVPgGgYr8ffj9PwReL2E"
+bot_token = "5662906950:AAEH-05hoRIr6V2_2y4KJZp5dDGLlSpewQg"
 
 # Telethon 
 client = TelegramClient('client', api_id, api_hash).start(bot_token=bot_token)
 #
-USERNAME = "Rahid_Tag_Bot"
-log_qrup = -1001864613336
+USERNAME = "CeyranTaggerBot"
+log_qrup = -1001823689827
 startmesaj = "\n\nMən qrupda bütün istifadəçiləri çağırmaq üçün tağ botuyam 🥰\n\nMəni qrupa əlavə edin sonra admin edərək tağ prosesi başlada bilərsən 👀"
 komutlar = "» /tag > 5 nəfərlik tağ edərəm 🙊\n\n» /tektag > Təkli tağ edərəm 🦦\n\n» /atag > Qrup adminlərini tağ edərəm 😁\n\n» /etag > Emojilər ilə tağ edərəm 🥰\n\n» /stag > Maraqlı sözlərlə tağ edərəm 😍\n\n» /cancel > Tağ prosesi dayandıraram 👀"
 qrupstart = "Mən işləyirəm 🥰\n\nBot əmrləri öyrənmək üçün şəxsimdə mənə yazın 👀"
-support = "qruzdaa"
-sahib = "Rahid_7"
+support = "NewModernBlog"
+sahib = "WinstonAndMee"
 #
 # samilben 
 # samilben 
