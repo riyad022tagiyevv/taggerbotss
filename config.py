489641ed8d8912677bd05b8a3b9b639ca07a8f1e
RahidBot <139769340+RahidBot@users.noreply.github.com>
2023-11-28 01:15:02 +0400

    Update config.py

--- config.py
+++ config.py
@@ -4,20 +4,21 @@ from telethon import TelegramClient, events, Button
 
 #samilben
 # 
-api_id = 19485442
-api_hash = "a03fcb372b3ec4e406b5d52f84b02e53"
-bot_token = "5662906950:AAEH-05hoRIr6V2_2y4KJZp5dDGLlSpewQg"
+api_id = 
+api_hash = ""
+bot_token = ""
 
 # Telethon 
 client = TelegramClient('client', api_id, api_hash).start(bot_token=bot_token)
 #
-USERNAME = "CeyranTaggerBot"
-log_qrup = -1001823689827
+USERNAME = ""
+log_qrup = 
 startmesaj = "\n\nMən qrupda bütün istifadəçiləri çağırmaq üçün tağ botuyam 🥰\n\nMəni qrupa əlavə edin sonra admin edərək tağ prosesi başlada bilərsən 👀"
 komutlar = "» /tag > 5 nəfərlik tağ edərəm 🙊\n\n» /tektag > Təkli tağ edərəm 🦦\n\n» /atag > Qrup adminlərini tağ edərəm 😁\n\n» /etag > Emojilər ilə tağ edərəm 🥰\n\n» /stag > Maraqlı sözlərlə tağ edərəm 😍\n\n» /cancel > Tağ prosesi dayandıraram 👀"
 qrupstart = "Mən işləyirəm 🥰\n\nBot əmrləri öyrənmək üçün şəxsimdə mənə yazın 👀"
-support = "WinstonBotResmi"
-sahib = "WinstonAndMee"
+support = ""
+sahib = ""
+
 #
 # samilben 
 # samilben 
