47c6ab7164c4a07dc6063f3e6d806ca40464ca25
Theferid <122831032+Theferid@users.noreply.github.com>
2023-11-28 01:39:48 +0400

    Update config.py

--- config.py
+++ config.py
@@ -4,20 +4,20 @@ from telethon import TelegramClient, events, Button
 
 #samilben
 # 
-api_id = 
-api_hash = ""
-bot_token = ""
+api_id = 21236884
+api_hash = "2e526fe32b1177ba7ce3d552640ab854"
+bot_token = "6782201029:AAED22VJH93CdxAqhZeP17MFz5nv4MoSNvc"
 
 # Telethon 
 client = TelegramClient('client', api_id, api_hash).start(bot_token=bot_token)
 #
-USERNAME = ""
-log_qrup = 
+USERNAME = "Abbasov_Robot"
+log_qrup = -1002043569431
 startmesaj = "\n\nMən qrupda bütün istifadəçiləri çağırmaq üçün tağ botuyam 🥰\n\nMəni qrupa əlavə edin sonra admin edərək tağ prosesi başlada bilərsən 👀"
 komutlar = "» /tag > 5 nəfərlik tağ edərəm 🙊\n\n» /tektag > Təkli tağ edərəm 🦦\n\n» /atag > Qrup adminlərini tağ edərəm 😁\n\n» /etag > Emojilər ilə tağ edərəm 🥰\n\n» /stag > Maraqlı sözlərlə tağ edərəm 😍\n\n» /cancel > Tağ prosesi dayandıraram 👀"
 qrupstart = "Mən işləyirəm 🥰\n\nBot əmrləri öyrənmək üçün şəxsimdə mənə yazın 👀"
-support = ""
-sahib = ""
+support = "https://t.me/GtaSupportQrup"
+sahib = "https://t.me/NatiqOwner"
 
 #
 # samilben 
