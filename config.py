868cb2058d96318cd25616958932592d3f1dc06e
Theferid <122831032+Theferid@users.noreply.github.com>
2023-12-01 18:11:29 +0400

    Update config.py

--- config.py
+++ config.py
@@ -4,20 +4,20 @@ from telethon import TelegramClient, events, Button
 
 #samilben
 # 
-api_id = 21236884
-api_hash = "2e526fe32b1177ba7ce3d552640ab854"
-bot_token = "6782201029:AAED22VJH93CdxAqhZeP17MFz5nv4MoSNvc"
+api_id = 26090016
+api_hash = "5b842f9801712684f2b98d70ead6538d"
+bot_token = "6532699211:AAF5UnQKxhof-LJPcRkCMjKqP2oefXPqj24"
 
 # Telethon 
 client = TelegramClient('client', api_id, api_hash).start(bot_token=bot_token)
 #
-USERNAME = "Abbasov_Robot"
-log_qrup = -1002043569431
+USERNAME = "GTAtaggerBot"
+log_qrup = -4096989129
 startmesaj = "\n\nMən qrupda bütün istifadəçiləri çağırmaq üçün tağ botuyam 🥰\n\nMəni qrupa əlavə edin sonra admin edərək tağ prosesi başlada bilərsən 👀"
 komutlar = "» /tag > 5 nəfərlik tağ edərəm 🙊\n\n» /tektag > Təkli tağ edərəm 🦦\n\n» /atag > Qrup adminlərini tağ edərəm 😁\n\n» /etag > Emojilər ilə tağ edərəm 🥰\n\n» /stag > Maraqlı sözlərlə tağ edərəm 😍\n\n» /cancel > Tağ prosesi dayandıraram 👀"
 qrupstart = "Mən işləyirəm 🥰\n\nBot əmrləri öyrənmək üçün şəxsimdə mənə yazın 👀"
-support = "https://t.me/GtaSupportQrup"
-sahib = "https://t.me/NatiqOwner"
+support = "https://t.me/sah_team"
+sahib = "https://t.me/The_ferid"
 
 #
 # samilben 
